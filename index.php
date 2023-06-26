2eaf8efb8517b9718ee90ce819d56a4e000ddd37
Franak007 <franck.pertosa@gmail.com>
2023-06-26 15:33:24 +0200

    save

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Service\PDOService;
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+dump(new PDOService());
--- src/service/PDOService.php
+++ src/service/PDOService.php
@@ -1,15 +1,51 @@
 <?php
 
-namespace App\service;
+namespace App\Service;
 
 use PDO;
 
 class PDOService
-
 {
-    protected PDO $pdo;
+    private PDO $PDO;
 
     private string $dsn = 'mysql:host=127.0.0.1:3306;dbname=allocinee_like';
     private string $user = 'root';
-    private string $pwd = '';
+    private string $password = '';
+
+    public function __construct()
+    {
+        $this->PDO = new PDO($this->dsn, $this->user, $this->password);
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPDO(): PDO
+    {
+        return $this->PDO;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDsn(): string
+    {
+        return $this->dsn;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 }
