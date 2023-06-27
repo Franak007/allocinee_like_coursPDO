a69eed396af556836de5bb5d7331819f69cf1bad
Franak007 <franck.pertosa@gmail.com>
2023-06-27 10:04:44 +0200

    save

--- index.php
+++ index.php
@@ -2,11 +2,9 @@
 include_once __DIR__ . '/vendor/autoload.php';
 
 use App\Service\PDOService;
-use App\Models\Movie;
-
-
 
 $PDO = new PDOService();
-// dump($PDO->findAllMovie());
+
+dump($PDO->findAllMovie());
 
 dump($PDO->findAll());
